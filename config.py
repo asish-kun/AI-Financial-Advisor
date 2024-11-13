246523aec9f11e8c8db0da553ac6f832c84a973a
Nithya <nithya.shree0916@gmail.com>
2024-11-13 15:33:26 -0500

    Backend code for User Authentication added

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://fin-data-db.documents.azure.com:443/'),
-    'master_key': os.environ.get('ACCOUNT_KEY', 'arneIEEzvee0hQFxog0li1WSGXpbZRK9axK4c0V2Alkwjr93DC7ZWU1KGH0WpWfXa8Rko9rSwncvACDbNdxbyQ=='),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
+    'host': os.environ.get('ACCOUNT_HOST'),
+    'master_key': os.environ.get('ACCOUNT_KEY'),
+    'database_id': os.environ.get('COSMOS_DATABASE'),
+    'container_id': os.environ.get('COSMOS_CONTAINER'),
 }
